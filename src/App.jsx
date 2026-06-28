6c58f9a4687e4778a8554cc02f78a7b212c6dc4b
okano-every <every.ting.criss6209@gmail.com>
2026-06-28 22:47:21 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Portal    from "./pages/Portal";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Portal from './pages/Portal'
+import Dashboard from './pages/Dashboard'
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/"          element={<Portal />} />
+        <Route path="/" element={<Portal />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* 今後のアプリをここに追加 */}
-        {/* <Route path="/fx"         element={<FxTracker />} /> */}
-        {/* <Route path="/education"  element={<Education />} /> */}
       </Routes>
-    </BrowserRouter>
-  );
+    </Router>
+  )
 }
+
+export default App
